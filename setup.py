be4bf2f07bb325761366da74a0fbe9ec49e822e2
nav5152-hub <tina5152@naver.com>
2026-05-02 07:57:07 +0100

    Add launch file

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_robot'
 
@@ -6,11 +8,12 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+   data_files=[
+    ('share/ament_index/resource_index/packages',
+        ['resource/my_robot']),
+    ('share/my_robot', ['package.xml']),
+    (os.path.join('share', 'my_robot', 'launch'), glob('launch/*.py')),
+],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='yunseo',
